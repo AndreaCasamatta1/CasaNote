74f6a1c07c28a09c61f5f7ab7e281edde95e406a
Andrea Casamatta <andrea.casamatta@CPT.local>
2025-02-12 10:48:24 +0100

    dev: commit con applicativo iniziale

---
+++ 5_Applicativo/CasaNote/src/main/java/ch/samt/CasaNote/CasaNoteApplication.java
@@ -0,0 +1,13 @@
+package ch.samt.CasaNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaNoteApplication.class, args);
+	}
+
+}
---
+++ 5_Applicativo/CasaNote/src/test/java/ch/samt/CasaNote/CasaNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.samt.CasaNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
