59d78575a44fc6d4b8d0a7f14c527d7cfad5e463
Andrea Casamatta <andrea.casamatta@CPT.local>
2025-02-19 09:39:54 +0100

    commit models

--- 5_Applicativo/CasaNote/controller/base.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask import request, render_template
-
-app = Blueprint('base', __name__)
-
-@app.route('/')
-def root():
-    return render_template('index.html')
