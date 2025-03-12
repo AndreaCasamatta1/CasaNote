08184b59727a9d3f4514c99b7e56e9eb31d8b434
paolo.comes <paolo.comes@outlook.com>
2025-03-12 10:38:44 +0100

    Creazione note, interfaccia web

--- 5_Applicativo/CasaNote/application/views/_templates/header.php
+++ 5_Applicativo/CasaNote/application/views/_templates/header.php
@@ -13,7 +13,14 @@
     <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
     <link rel="stylesheet" href= "application/libs/bootstrap/css/bootstrap.min.css">
     <script src="application/libs/bootstrap/js/bootstrap.min.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        .canvas-container {
+            border: 1px solid #ccc;
+            width: 100%;
+            height: 300px;
+        }
+    </style>
 </head>
 <?php
 $color = isset($_COOKIE['color']) ? '#' . $_COOKIE['color'] : '#FFFFFF'; // Default: bianco
