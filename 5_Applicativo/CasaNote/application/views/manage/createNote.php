33ac4028622def14d8dfd6141b92f7add741f3c1
paolo.comes <paolo.comes@samtrevano.ch>
2025-03-12 15:19:05 +0100

    af

--- 5_Applicativo/CasaNote/application/views/manage/createNote.php
+++ 5_Applicativo/CasaNote/application/views/manage/createNote.php
@@ -1,25 +1,38 @@
 <script src="application/views/_templates/static/js/manageInput.js"></script>
-<div class="m-2 p-2">
-    <form id="create-form" method="POST">
-        <div class="pt-4"></div>
-
-        <div class="form-group">
-            <label for="title">Titolo</label>
-            <input type="text" class="form-control" id="title" placeholder="Inserisci il titolo">
-        </div>
-        <div class="form-group">
-            <button type="button" class="btn btn-outline-primary position-fixed m-3" data-bs-toggle="dropdown">
-                +
-            </button>
-            <ul class="dropdown-menu" id="add-option">
-                <li><a class="dropdown-item" href="#" onclick="addInput('text')">Text</a></li>
-                <li><a class="dropdown-item" href="#" onclick="addInput('attachment')">Attachment</a></li>
-                <li><a class="dropdown-item" href="#" onclick="addInput('draw')">Draw</a></li>
+<form method="POST" action="manage/saveOrUpdateNote">
+    <nav class="navbar navbar-expand-lg m-0">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"><img src="application/libs/img/logo.png" width="30" height="30" alt=""></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <button type="submit" class="btn btn-primary">Salva</button>
+                </li>
+                <li class="nav-item">
+                    <div class="form-group">
+                        <button type="button" class="btn btn-outline-primary" data-bs-toggle="dropdown">
+                            +
+                        </button>
+                        <ul class="dropdown-menu" id="add-option">
+                            <li><a class="dropdown-item" onclick="addInput('text')">Text</a></li>
+                            <li><a class="dropdown-item" onclick="addInput('attachment')">Attachment</a></li>
+                            <li><a class="dropdown-item" onclick="addInput('draw')">Draw</a></li>
+                        </ul>
+                    </div>
+                </li>
             </ul>
         </div>
+    </nav>
 
-        <div id="dynamic-fields"></div>
-        <button type="submit" class="btn btn-primary position-fixed bottom-0 end-0 m-3">Salva</button>
+    <div class="form-group">
+        <label for="title">Titolo</label>
+        <input type="text" name="title" class="form-control" id="title" placeholder="Inserisci il titolo">
+    </div>
+    <br>
+    <br>
+    <div id="dynamic-fields"></div>
+</form>
 
-    </form>
-</div>
+
+
