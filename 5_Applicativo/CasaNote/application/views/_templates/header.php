a00a3d8ae97ae8d76c0ed47b963842794a1d98f6
AndreaCasamatta1 <160742278+AndreaCasamatta1@users.noreply.github.com>
2025-05-07 13:49:55 +0200

    aggiornatoo sicurezza dei link

--- 5_Applicativo/CasaNote/application/views/_templates/header.php
+++ 5_Applicativo/CasaNote/application/views/_templates/header.php
@@ -15,6 +15,7 @@
     <script src="application/libs/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="icon" type="image/vnd.icon" href="/application/libs/img/favLogo.ico">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <!-- Include Font Awesome per le icone -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
